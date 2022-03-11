f6482267c3afd58fcc957d0d7d63e3fbfcebf8f2
Amit Jena <amitjenaiitbm@gmail.com>
2022-03-11 20:01:35 +0530

    a lot added

--- comparable_dnp_english.py
+++ comparable_dnp_english.py
@@ -4,21 +4,306 @@ import os
 #Python libraries
 from fpdf import FPDF
 
+WIDTH = 210
+HEIGHT = 297
+
 def create_first_page(pdf):
     #Top banner
     pdf.image("./resources/topBanner-1.png", x=0, y=0, w=WIDTH)
 
-def create_second_page(pdf):
+    # Add "District Nutrition Profile" text
+    pdf.set_font('Roboto-Bold', 'B', 20)
+    pdf.set_text_color(255, 255, 255)
+    banner_text = "district nutrition profile"
+    pdf.set_xy(100, 6)
+    pdf.cell(80, 20, banner_text.upper())
 
+    # Add orange rectangle banner
+    pdf.set_draw_color(231, 121, 37)
+    pdf.set_fill_color(231, 121, 37)
+    pdf.rect(x=0, y=40.5, w=WIDTH, h=10, style='F')
+
+    # Add District, State and Month
+    pdf.set_font('Roboto-Bold', 'B', 13)
+    pdf.set_text_color(255, 255, 255)
+    orangeBanner_text = district[3].upper() + " | " + district[1].upper()
+    pdf.set_xy(7, 41)
+    pdf.cell(186, 10, orangeBanner_text, 0, 0, 'L')
+    pdf.cell(10, 10, 'MARCH 2022', 0, 0, 'R')
+
+    # About DNPs: Title
+    pdf.set_font('Roboto-Bold', 'B', 13)
+    pdf.set_text_color(231, 121, 37)
+    pdf.set_xy(7, 54)
+    aboutDNP_text = "About District Nutrition Profiles:"
+    pdf.cell(100, 10, aboutDNP_text, 0, 0, 'L')
+    # About DNPs: Description text
+    pdf.set_font('Roboto-Regular', '', 10)
+    pdf.set_text_color(0, 0, 0)
+    pdf.set_xy(7, 63)
+    DNPdesc_text = "District Nutrition Profiles (DNPs) are available for 707 districts in India. They present trends for key nutrition and health outcomes and their cross-sectoral determinants in a district. The DNPs are based on data from the National Family Health Survey (NFHS)-4 (2015-2016) and NFHS-5 (2019-2020). They are aimed primarily at district administrators, state functionaries, local leaders, and development actors working at the district-level."
+    pdf.multi_cell(115, 4.5, DNPdesc_text, 0, 'J')
+
+    # Add district map
+    map_path = './data/maps/comparable_maps/{}.jpg'.format(district[4])
+    pdf.image(map_path, x=125, y=53, w=75, h=40)
+    # map caption
+    figure1_text = "Figure 1:"
+    mapCaption_text = "Map highlights district {} in the state/UT of {}".format(district[3], district[1])
+    pdf.set_text_color(0, 0, 0)
+    pdf.set_font('Roboto-Bold', 'B', 7)
+    pdf.set_xy(125, 92)
+    pdf.cell(9, 7, figure1_text, 0, 0, 'L')
+    pdf.set_xy(125+9+1.5, 92)
+    pdf.set_font('Roboto-Regular', '', 7)
+    pdf.cell(60, 7, mapCaption_text[0:55]+'-', 0, 'L')
+    pdf.set_xy(125, 95)
+    pdf.cell(70, 7, mapCaption_text[55:], 0, 'L')
+
+    # Add framework as image
+    pdf.image("./resources/framework.png", x=8, y=105, w=90)
+    # Add framework caption
+    source_text = "Source:"
+    frameworkCaption_text = "Adapted from Black et al. (2008)"
+    pdf.set_text_color(0, 0, 0)
+    pdf.set_font('Roboto-Bold', 'B', 7)
+    pdf.set_xy(8, 170)
+    pdf.cell(7, 7, source_text, 0, 0, 'L')
+    pdf.set_xy(8+7+2, 170)
+    pdf.set_font('Roboto-Regular', '', 7)
+    pdf.cell(60, 7, frameworkCaption_text, 0, 'L')
+
+    # Factors for child undernutrition: Title
+    pdf.set_font('Roboto-Bold', 'B', 13)
+    pdf.set_text_color(231, 121, 37)
+    pdf.set_xy(100, 100)
+    undernutrition_text = "What factors lead to child undernutrition?"
+    pdf.cell(100, 10, undernutrition_text, 0, 0, 'L')
+    # Child undernutrition: Description text
+    pdf.set_font('Roboto-Regular', '', 10)
+    pdf.set_text_color(0, 0, 0)
+    pdf.set_xy(100, 110)
+    undernutritionDesc_text = "Given the focus of India’s national nutrition mission on child undernutrition , the DNPs focus in on the determinants of child undernutrition (Figure on the left). Multiple determinants of suboptimal child nutrition and development contribute to the outcomes seen at the district-level. Different types of interventions can influence these determinants. Immediate determinants include inadequacies in food, health, and care for infants and young children, especially in the first two years of life. Nutrition-specific interventions such as health service delivery at the right time during pregnancy and early childhood can affect immediate determinants. Underlying and basic determinants include women’s status, household food security, hygiene, and socio-economic conditions. Nutrition-sensitive interventions such as social safety nets, sanitation programs, women’s empowerment, and agriculture programs can affect underlying and basic determinants."
+    pdf.multi_cell(102, 4.5, undernutritionDesc_text, 0, 'J')
+
+    # Add grey rectangle
+    pdf.set_draw_color(109, 111, 113)
+    pdf.set_fill_color(109, 111, 113)
+    pdf.rect(x=7, y=184, w=WIDTH-14, h=10, style='F')
+    # Add text into the grey bar
+    greyBar_text = "District demographic profile, 2019-20"
+    pdf.set_font('Roboto-Bold', 'B', 13)
+    pdf.set_text_color(255, 255, 255)
+    pdf.set_xy(10, 184)
+    pdf.cell(100, 10, greyBar_text, 0, 0, 'L')
+    pdf.set_font('Roboto-Bold', 'B', 12)
+    pdf.cell(90, 10, district[3], 0, 0, 'R')
+
+    # State nutrition profiles
+    pdf.image("./resources/state_nut_prof.png", x=7, y=195, w=WIDTH-14)
+    # Sex ratio number
+    pdf.set_font('Roboto-Bold', 'B', 28)
+    pdf.set_text_color(255, 255, 255)
+    pdf.set_xy(20, 194)
+    pdf.cell(40, 20, str("{:,}".format(round(float(district[5]))))+"/1,000", 0, 0, 'L')
+    # Sex ratio text
+    sexRatio_text1 = "Sex ratio (females per 1,000"
+    sexRatio_text2 = "males) of the total population"
+    pdf.set_font('Roboto-Bold', 'B', 10)
+    pdf.set_text_color(255, 255, 255)
+    pdf.set_xy(20, 207)
+    pdf.cell(55, 10, sexRatio_text1, 0, 0, 'L')
+    pdf.set_xy(20, 211)
+    pdf.cell(55, 10, sexRatio_text2, 0, 0, 'L')
+    # Reproductive age number
+    pdf.set_font('Roboto-Bold', 'B', 28)
+    pdf.set_text_color(255, 255, 255)
+    pdf.set_xy(85, 194)
+    pdf.cell(40, 20, str("{:,}".format(round(float(district[6])))), 0, 0, 'L')
+    # Reproductive age text
+    reproductive_text1 = "Number of women of"
+    reproductive_text2 = "reproductive age (15–49 yrs)"
+    pdf.set_font('Roboto-Bold', 'B', 10)
+    pdf.set_text_color(255, 255, 255)
+    pdf.set_xy(85, 207)
+    pdf.cell(55, 10, reproductive_text1, 0, 0, 'L')
+    pdf.set_xy(85, 211)
+    pdf.cell(55, 10, reproductive_text2, 0, 0, 'L')
+    # Pregnant women number
+    pdf.set_font('Roboto-Bold', 'B', 28)
+    pdf.set_text_color(255, 255, 255)
+    pdf.set_xy(152, 194)
+    pdf.cell(40, 20, str("{:,}".format(round(float(district[7])))), 0, 0, 'L')
+    # Pregnant women text
+    pregnantWomen_text1 = "Number of"
+    pregnantWomen_text2 = "pregnant women"
+    pdf.set_font('Roboto-Bold', 'B', 10)
+    pdf.set_text_color(255, 255, 255)
+    pdf.set_xy(152, 207)
+    pdf.cell(55, 10, pregnantWomen_text1, 0, 0, 'L')
+    pdf.set_xy(152, 211)
+    pdf.cell(55, 10, pregnantWomen_text2, 0, 0, 'L')
+    # Live birth number
+    pdf.set_font('Roboto-Bold', 'B', 28)
+    pdf.set_text_color(255, 255, 255)
+    pdf.set_xy(20, 221)
+    pdf.cell(40, 20, str("{:,}".format(round(float(district[8])))), 0, 0, 'L')
+    # Live birth text
+    liveBirth_text1 = "Number of live births"
+    pdf.set_font('Roboto-Bold', 'B', 10)
+    pdf.set_text_color(255, 255, 255)
+    pdf.set_xy(20, 234)
+    pdf.cell(55, 10, liveBirth_text1, 0, 0, 'L')
+    # Total children number
+    pdf.set_font('Roboto-Bold', 'B', 28)
+    pdf.set_text_color(255, 255, 255)
+    pdf.set_xy(85, 221)
+    pdf.cell(40, 20, str("{:,}".format(round(float(district[9])))), 0, 0, 'L')
+    # Total children text
+    totalChildren_text1 = "Total number of children"
+    totalChildren_text2 = "under 5 yrs"
+    pdf.set_font('Roboto-Bold', 'B', 10)
+    pdf.set_text_color(255, 255, 255)
+    pdf.set_xy(85, 234)
+    pdf.cell(55, 10, totalChildren_text1, 0, 0, 'L')
+    pdf.set_xy(85, 238)
+    pdf.cell(55, 10, totalChildren_text2, 0, 0, 'L')
+    # Births registered number
+    pdf.set_font('Roboto-Bold', 'B', 28)
+    pdf.set_text_color(255, 255, 255)
+    pdf.set_xy(152, 221)
+    pdf.cell(40, 20, str("{:,}".format(round(float(district[10])))), 0, 0, 'L')
+    # Births registered text
+    birthsRegistered_text1 = "Number of"
+    birthsRegistered_text2 = "pregnant women"
+    pdf.set_font('Roboto-Bold', 'B', 10)
+    pdf.set_text_color(255, 255, 255)
+    pdf.set_xy(152, 234)
+    pdf.cell(55, 10, birthsRegistered_text1, 0, 0, 'L')
+    pdf.set_xy(152, 238)
+    pdf.cell(55, 10, birthsRegistered_text2, 0, 0, 'L')
+
+    # source
+    pdf.set_font('Roboto-Bold', 'B', 7)
+    pdf.set_text_color(0, 0, 0)
+    pdf.set_xy(8, 249)
+    pdf.cell(50, 3, "Source:", align='L')
+    source1_text = "1. IFPRI estimates - The headcount was calculated as the product of the undernutrition prevalence and the total eligible projected population for each district in 2019. Projected population for 2019 was estimated using Census 2011."
+    pdf.set_font('Roboto-Regular', '', 7)
+    pdf.set_xy(8, 252)
+    pdf.cell(180, 3, source1_text[0:178], align='L')
+    pdf.set_xy(10.5, 255)
+    pdf.cell(180, 3, source1_text[179:], align='L')
+    source2_text = "2. NFHS-4 (2015-16) & NFHS-5 district & state factsheets (2019-20)."
+    pdf.set_xy(8, 258)
+    pdf.cell(200, 3, source2_text, align='L')
+
+    # Horizontal line
+    pdf.set_draw_color(109, 111, 113)
+    pdf.set_line_width(0.75)
+    pdf.line(0,262, WIDTH, 262)
+
+    # citation
+    citation_text = "Citation:"
+    citation_text1 = "Singh. N., P.H. Nguyen, M. Jangid, S.K. Singh, R. Sarwal, N. Bhatia, R. Johnston, W. Joe, and P. Menon. 2022. District Nutrition Profile: {}, {}. New Delhi, India: International Food Policy Research Institute.".format(district[3], district[1])
+    pdf.set_text_color(0, 0, 0)
+    pdf.set_font('Roboto-Bold', 'B', 7)
+    pdf.set_xy(8, 264)
+    pdf.cell(9, 3, citation_text, 0, 0, 'L')
+    pdf.set_xy(8+9+1, 264)
+    pdf.set_font('Roboto-Regular', '', 7)
+    pdf.cell(170, 3, citation_text1[0:167]+'-', 0, 'L')
+    pdf.set_xy(8, 267)
+    pdf.cell(170, 3, citation_text1[167:], 0, 'L')
+
+    # Acknowledgement
+    acknowledgement_text = "Acknowledgement:"
+    acknowledgement_text1 = "Financial support was provided by the Bill & Melinda Gates Foundation through POSHAN, led by the International Food Policy Research Institute. We thank Amit Jena (Independent Researcher) for design and programming support."
+    pdf.set_text_color(0, 0, 0)
+    pdf.set_font('Roboto-Bold', 'B', 7)
+    pdf.set_xy(8, 271)
+    pdf.cell(9, 3, acknowledgement_text, 0, 0, 'L')
+    pdf.set_xy(8+21+0.5, 271)
+    pdf.set_font('Roboto-Regular', '', 7)
+    pdf.cell(155, 3, acknowledgement_text1[0:152], 0, 'L')
+    pdf.set_xy(8, 274)
+    pdf.cell(155, 3, acknowledgement_text1[152:], 0, 'L')
+
+    # Logos
+    img_width = 120
+    pdf.image("./resources/logos.png", x=(WIDTH-img_width)/2+1, y=278, w=img_width)
+
+def create_second_page(pdf):
+    # Add top bar
+    pdf.set_draw_color(41, 119, 115)
+    pdf.set_fill_color(41, 119, 115)
+    pdf.rect(x=7, y=5, w=WIDTH-14, h=10, style='F')
+    # Add text into the top bar
+    top_Bar2_text = "The state of nutrition outcomes among children (<5 years)"
+    pdf.set_font('Roboto-Bold', 'B', 13)
+    pdf.set_text_color(255, 255, 255)
+    pdf.set_xy(10, 5)
+    pdf.cell(100, 10, top_Bar2_text, 0, 0, 'L')
+    pdf.set_font('Roboto-Bold', 'B', 12)
+    pdf.cell(90, 10, district[3], 0, 0, 'R')
+
+    # Add bottom bar
+    pdf.set_draw_color(41, 119, 115)
+    pdf.set_fill_color(41, 119, 115)
+    pdf.rect(x=7, y=150, w=WIDTH-14, h=10, style='F')
+    # Add text into the bottom bar
+    bottom_Bar2_text = "The state of nutrition outcomes among women (15-49 years)"
+    pdf.set_font('Roboto-Bold', 'B', 13)
+    pdf.set_text_color(255, 255, 255)
+    pdf.set_xy(10, 150)
+    pdf.cell(100, 10, bottom_Bar2_text, 0, 0, 'L')
+    pdf.set_font('Roboto-Bold', 'B', 12)
+    pdf.cell(90, 10, district[3], 0, 0, 'R')
 
 def create_third_page(pdf):
+    # Add top bar
+    pdf.set_draw_color(0, 96, 162)
+    pdf.set_fill_color(0, 96, 162)
+    pdf.rect(x=7, y=5, w=WIDTH-14, h=10, style='F')
+    # Add text into the top bar
+    top_Bar3_text = "Immediate determinants"
+    pdf.set_font('Roboto-Bold', 'B', 13)
+    pdf.set_text_color(255, 255, 255)
+    pdf.set_xy(10, 5)
+    pdf.cell(100, 10, top_Bar3_text, 0, 0, 'L')
+    pdf.set_font('Roboto-Bold', 'B', 12)
+    pdf.cell(90, 10, district[3], 0, 0, 'R')
 
+    # Add bottom bar
+    pdf.set_draw_color(152, 56, 87)
+    pdf.set_fill_color(152, 56, 87)
+    pdf.rect(x=7, y=150, w=WIDTH-14, h=10, style='F')
+    # Add text into the bottom bar
+    bottom_Bar3_text = "Underlying determinants"
+    pdf.set_font('Roboto-Bold', 'B', 13)
+    pdf.set_text_color(255, 255, 255)
+    pdf.set_xy(10, 150)
+    pdf.cell(100, 10, bottom_Bar3_text, 0, 0, 'L')
+    pdf.set_font('Roboto-Bold', 'B', 12)
+    pdf.cell(90, 10, district[3], 0, 0, 'R')
 
 def create_fourth_page(pdf):
-
+    # Add top bar
+    pdf.set_draw_color(91, 83, 134)
+    pdf.set_fill_color(91, 83, 134)
+    pdf.rect(x=7, y=5, w=WIDTH-14, h=10, style='F')
+    # Add text into the top bar
+    top_Bar4_text = "Trends in coverage of interventions across the first 1,000 days"
+    pdf.set_font('Roboto-Bold', 'B', 13)
+    pdf.set_text_color(255, 255, 255)
+    pdf.set_xy(10, 5)
+    pdf.cell(100, 10, top_Bar4_text, 0, 0, 'L')
+    pdf.set_font('Roboto-Bold', 'B', 12)
+    pdf.cell(90, 10, district[3], 0, 0, 'R')
+    
 
 def create_report(filename):
-    pdf = FPDF()
+    pdf = FPDF('P', 'mm', 'A4')
 
     #Add custom fonts
     pdf.add_font('Roboto-Regular', '',
@@ -46,21 +331,17 @@ def create_report(filename):
 
     #create a sub-folder
     path = 'generated_dnp/comparable_dnp_english'
-    dirName = path + '/{}'.format(state)
+    dirName = path + '/{}'.format(district[1])
     if not os.path.exists(dirName):
         os.mkdir(dirName)
     pdf.output(dirName+'/'+filename, "F")
 
 #Read CSV file
-with open("state_2016.csv", 'r') as infile:
+with open("./data/csv/comparable_district_data_temp.csv", 'r') as infile:
     reader = csv.reader(infile, delimiter=",")
     header = next(reader)
-    for row in reader:
-        state = row[0]
-        district = "Jena"
-        children_stunted = row[1]
-        children_wasted = row[2]
-
+    for district in reader:
+        print(district)
         #Generated DNP file name
-        filename="{}-{}".format(district, state)
+        filename="{}-{}.pdf".format(district[3], district[1])
         create_report(filename)
---
+++ flippy.py
@@ -0,0 +1,277 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Generate flip-books from videos and animated GIFs
+#
+# Copyright © 2016-2020 Oliver Lau <ola@ct.de>, Heise Medien GmbH & Co. KG
+# All rights reserved.
+
+import os
+import sys
+import argparse
+from PIL import Image, GifImagePlugin
+from fpdf import FPDF
+# from moviepy.editor import *
+
+
+class Size:
+    """ Class to store the size of a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    def to_tuple(self):
+        return self.width, self.height
+
+    def __str__(self):
+        return f'Size({self.width}x{self.height})'
+
+    @staticmethod
+    def from_tuple(sz):
+        return Size(sz[0], sz[1])
+
+
+class Point:
+    """ Class to store a point on a 2D plane."""
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f'Point({self.x}, {self.y})'
+
+
+class Margin:
+    """ Class to store the margins of a rectangular boundary."""
+
+    def __init__(self, top, right, bottom, left):
+        self.top = top
+        self.right = right
+        self.bottom = bottom
+        self.left = left
+
+    def __str__(self):
+        return f'Margin({self.top}, {self.right}, {self.bottom}, {self.left})'
+
+
+class AnimatedGif:
+    """ Generator for a sequence of Image objects from an animated GIF """
+
+    def __init__(self, im):
+        self.im = im
+
+    def __getitem__(self, ix):
+        try:
+            if ix:
+                self.im.seek(ix)
+            return self.im
+        except EOFError:
+            raise IndexError
+
+    def open(self, file_name):
+        self.im = Image.open(file_name)
+
+
+class FlipbookCreator:
+    PAPER_SIZES = {
+        'a5': Size(210, 148),
+        'a4': Size(297, 210),
+        'a3': Size(420, 297),
+        'letter': Size(279.4, 215.9),
+        'legal': Size(355.6, 215.9)
+    }
+    PAPER_CHOICES = PAPER_SIZES.keys()
+
+    def __init__(self, verbosity=0, input_file_name=''):
+        self.verbosity = verbosity
+        self.input_file_name = input_file_name
+        self.frames = None
+        self.clip = None
+        if self.input_file_name.endswith('.gif'):
+            self.im = Image.open(self.input_file_name)
+            self.palette = self.im.getpalette()
+            self.frames = AnimatedGif(self.im)
+            self.frame_count = len(list(AnimatedGif(self.im)))
+            self.fps = 0
+            self.last_im = Image.new('P', self.im.size)
+            for i in range(0, 2):
+                for f in AnimatedGif(self.im):
+                    self.last_im.putpalette(self.palette)
+                    #  im = Image.alpha_composite(self.last_im, f.convert('RGBA'))
+                    im = self.last_im.copy()
+                    im.paste(f)
+                    self.last_im = im.copy()
+        else:
+            self.clip = VideoFileClip(self.input_file_name)
+            self.fps = self.clip.fps
+            self.frame_count = int(self.clip.duration * self.clip.fps)
+        if self.verbosity > 0:
+            print(f'Opening {self.input_file_name} ...')
+
+    def process(self,
+                output_file_name=None,
+                dpi=150,
+                offset=0,
+                fps=10,
+                height_mm=50,
+                margins=Margin(10, 10, 10, 10),
+                paper_format='a4'):
+
+        def draw_raster():
+            for ix in range(0, nx + 1):
+                xx = x0 + ix * total.width
+                pdf.line(xx, y0, xx, y1)
+                if offset > 0 and ix != nx:
+                    pdf.line(xx + offset, y0, xx + offset, y1)
+            for iy in range(0, ny + 1):
+                yy = y0 + iy * total.height
+                pdf.line(x0, yy, x1, yy)
+
+        height_mm = float(height_mm)
+        tmp_files = []
+        if self.clip:
+            if fps != self.clip.fps:
+                if self.verbosity > 0:
+                    print(f'Transcoding from {self.clip.fps} fps to {fps} fps ...')
+                self.clip.write_videofile('tmp.mp4', fps=fps, audio=False)
+                tmp_files.append('tmp.mp4')
+                self.clip = VideoFileClip('tmp.mp4')
+                self.fps = self.clip.fps
+                self.frame_count = int(self.clip.duration * self.fps)
+            clip_size = Size.from_tuple(self.clip.size)
+        elif self.frames:
+            clip_size = Size.from_tuple(self.im.size)
+
+        paper = self.PAPER_SIZES[paper_format.lower()]
+        printable_area = Size(paper.width - margins.left - margins.right,
+                              paper.height - margins.top - margins.bottom)
+        frame_mm = Size(height_mm / clip_size.height * clip_size.width, height_mm)
+        total = Size(offset + frame_mm.width, frame_mm.height)
+        frame = Size(int(frame_mm.width / 25.4 * dpi),
+                     int(frame_mm.height / 25.4 * dpi))
+        nx = int(printable_area.width / total.width)
+        ny = int(printable_area.height / total.height)
+        if self.verbosity > 0:
+            print('Input:  {} fps, {}x{}, {} frames'\
+                '\n        from: {}'\
+                .format(
+                    self.fps,
+                    clip_size.width,
+                    clip_size.height,
+                    self.frame_count,
+                    self.input_file_name
+                ))
+            print('Output: {}dpi, {}x{}, {:.2f}mm x {:.2f}mm, {}x{} tiles'\
+                '\n        to: {}'\
+                .format(
+                    dpi,
+                    frame.width, frame.height,
+                    frame_mm.width, frame_mm.height,
+                    nx, ny,
+                    output_file_name
+                ))
+        pdf = FPDF(unit='mm', format=paper_format.upper(), orientation='L')
+        pdf.set_compression(True)
+        pdf.set_title('Funny video')
+        pdf.set_author('Oliver Lau <ola@ct.de> - Heise Medien GmbH & Co. KG')
+        pdf.set_creator('flippy')
+        pdf.set_keywords('flip-book, video, animated GIF')
+        pdf.set_draw_color(128, 128, 128)
+        pdf.set_line_width(0.1)
+        pdf.set_font('Helvetica', '', 12)
+        pdf.add_page()
+        i = 0
+        page = 0
+        tx, ty = -1, 0
+        x0, y0 = margins.left, margins.top
+        x1, y1 = x0 + nx * total.width, y0 + ny * total.height
+
+        if self.clip:
+            all_frames = self.clip.iter_frames()
+        elif self.frames:
+            all_frames = AnimatedGif(self.im)
+        else:
+            all_frames = []
+        for f in all_frames:
+            ready = float(i + 1) / self.frame_count
+            if self.verbosity:
+                sys.stdout.write('\rProcessing frames |{:30}| {}%'
+                                 .format('X' * int(30 * ready), int(100 * ready)))
+                sys.stdout.flush()
+            tx += 1
+            if type(f) == GifImagePlugin.GifImageFile:
+                f.putpalette(self.palette)
+                self.last_im.paste(f)
+                im = self.last_im.convert('RGBA')
+            else:
+                im = Image.fromarray(f)
+                im.thumbnail(frame.to_tuple())
+            if tx == nx:
+                tx = 0
+                ty += 1
+                if ty == ny:
+                    ty = 0
+                    draw_raster()
+                    pdf.add_page()
+                    page += 1
+            temp_file = 'tmp-{}-{}-{}.png'.format(page, tx, ty)
+            im.save(temp_file)
+            tmp_files.append(temp_file)
+            x = x0 + tx * total.width
+            y = y0 + ty * total.height
+            pdf.image(temp_file,
+                      x=x + offset,
+                      y=y,
+                      w=frame_mm.width,
+                      h=frame_mm.height)
+            text = Point(x, y + frame_mm.height - 2)
+            if offset > 0:
+                pdf.rotate(90, text.x, text.y)
+                pdf.text(text.x, text.y + 5, '{}'.format(i))
+                pdf.rotate(0)
+            i += 1
+
+        if y != 0 and x != 0:
+            draw_raster()
+
+        if self.verbosity > 0:
+            print('\nGenerating PDF ...')
+        pdf.output(name=output_file_name)
+        if self.verbosity > 0:
+            print('Removing temporary files ...')
+        for temp_file in tmp_files:
+            os.remove(temp_file)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Generate flip-books from videos.')
+    parser.add_argument('video', type=str, help='File name of video/GIF to process')
+    parser.add_argument('--out', type=str, help='Name of PDF file to write to', default='flip-book.pdf')
+    parser.add_argument('--height', type=float, help='Height of flip-book [mm]', default=30)
+    parser.add_argument('--paper', type=str, choices=FlipbookCreator.PAPER_CHOICES, help='paper size.', default='a4')
+    parser.add_argument('--offset', type=float, help='Margin left to each frame [mm]', default=15.0)
+    parser.add_argument('--phena', action='store_true', help='Create PDF to use in Phenakistoscope')
+    parser.add_argument('--dpi', type=int, help='DPI', default=200)
+    parser.add_argument('--fps', type=int, help='Frames per second', default=10)
+    parser.add_argument('-v', type=int, nargs='?', help='verbosity level', default=1)
+    args = parser.parse_args()
+
+    if args.phena:
+        print('Phenakistoscope not supported yet.')
+        sys.exit(1)
+
+    flippy = FlipbookCreator(
+        input_file_name=args.video,
+        verbosity=args.v)
+    flippy.process(
+        paper_format=args.paper,
+        output_file_name=args.out,
+        height_mm=args.height,
+        dpi=args.dpi,
+        offset=args.offset
+    )
+
+if __name__ == '__main__':
+    main()
